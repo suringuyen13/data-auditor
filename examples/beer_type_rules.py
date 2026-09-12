d1849b91cc22e685008e755482b31fad1664fdaa
suringuyen13 <thunguyen2132007@gmail.com>
2026-09-12 16:59:38 -0400

    add benchmark dataset

---
+++ examples/beer_type_rules.py
@@ -0,0 +1,35 @@
+type_rules = {
+    "index": {
+        "type": "integer"
+    },
+    "id": {
+        "type": "integer"
+    },
+    "beer_name": {
+        "type": "string"
+    },
+    "style": {
+        "type": "string"
+    },
+    "ounces": {
+        "type": "numeric"
+    },
+    "abv": {
+        "type": "numeric"
+    },
+    "ibu": {
+        "type": "numeric"
+    },
+    "brewery_id": {
+        "type": "integer"
+    },
+    "brewery_name": {
+        "type": "string"
+    },
+    "city": {
+        "type": "string"
+    },
+    "state": {
+        "type": "string"
+    }
+}
